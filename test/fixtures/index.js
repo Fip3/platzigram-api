4557c690c783aa6c00c46f9b0ad47856b0703b14
Felipe Torrejon <felipetorrejon@live.cl>
2017-02-10 20:49:19 -0300

    clase70 - creacion de endpoint GET/picture

---
+++ test/fixtures/index.js
@@ -0,0 +1,13 @@
+export default {
+  getImage () {
+    return {
+      id: '59428604968',
+      publicId: '4ZJ2pwDkMjMFjOi',
+      userId: 'platzigram',
+      src: 'http://platzigram.test/4ZJ2pwDkMjMFjOi.jpg',
+      description: '#awesome',
+      tags: [ 'awesome' ],
+      createdAt: new Date().toString()
+    }
+  }
+}
--- test/pictures-test.js
+++ test/pictures-test.js
@@ -2,25 +2,26 @@
 
 import test from 'ava'
 import micro from 'micro'
-import uuid from 'uuid-base62'
+// import uuid from 'uuid-base62'
 import listen from 'test-listen'
 import request from 'request-promise'
+import fixtures from './fixtures'
 import pictures from '../pictures'
 
 test('GET /:id', async t => {
-  let id = uuid.v4()
+  // let id = uuid.v4()
+  let image = fixtures.getImage()
 
   /*
   let srv = micro(async (req, res) => {
     send(res, 200, { id })
   })
   */
-
   let srv = micro(pictures)
-
   let url = await listen(srv)
-  let body = await request({ uri: `${url}/${id}`, json: true })
-  t.deepEqual(body, { id })
+
+  let body = await request({ uri: `${url}/${image.publicId}`, json: true })
+  t.deepEqual(body, image)
 })
 
 test.todo('POST /')
