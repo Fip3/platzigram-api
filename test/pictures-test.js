b6b854fc7a01969836e4d321eedb42fdfe851757
Felipe Torrejon <felipetorrejon@live.cl>
2017-02-13 18:37:59 -0300

    clase71 - creación de endpoint POST/picture

--- test/pictures-test.js
+++ test/pictures-test.js
@@ -8,6 +8,11 @@ import request from 'request-promise'
 import fixtures from './fixtures'
 import pictures from '../pictures'
 
+test.beforeEach(async t => {
+  let srv = micro(pictures)
+  t.context.url = await listen(srv)
+})
+
 test('GET /:id', async t => {
   // let id = uuid.v4()
   let image = fixtures.getImage()
@@ -17,12 +22,34 @@ test('GET /:id', async t => {
     send(res, 200, { id })
   })
   */
-  let srv = micro(pictures)
-  let url = await listen(srv)
+  // let srv = micro(pictures)
+
+  let url = t.context.url
 
   let body = await request({ uri: `${url}/${image.publicId}`, json: true })
   t.deepEqual(body, image)
 })
 
-test.todo('POST /')
+test('POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    resolveWithFullResponse: true
+  }
+
+  let response = await request(options)
+
+  t.is(response.statusCode, 201)
+  t.deepEqual(response.body, image)
+})
+
 test.todo('POST /:id/like')
